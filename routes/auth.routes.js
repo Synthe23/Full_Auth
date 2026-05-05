396f3a79af062a108d1176983211c7a5706946bd
OmPrakash23 <mishraomm5000@gmail.com>
2026-05-05 19:44:58 +0530

    Add authentication routes for user management

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,1 +1,41 @@
+import { Router } from "express";
+import {
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  getMeController,
+} from "../controllers/auth.controllers.js";
+import {authMiddleware} from "../middleware/auth.middleware.js";
+
+const authRouter = Router();
+
+/**
+ * @route - POST /api/auth/register
+ * @description Register the user
+ * @access public
+ */
+authRouter.post("/register", registerUserController);
+
+/**
+ * @route - POST /api/auth/login
+ * @description login existing user
+ * @access public
+ */
+authRouter.post("/login", loginUserController);
+
+/**
+ * @route - GET /api/auth/logout
+ * @description Clear the token from the user's cookie and add it to blacklist model
+ * @access private
+ */
+authRouter.post("/logout", authMiddleware, logoutUserController);
+
+/**
+ * @route - GET /api/auth/get-me
+ * @description Get the current logged-in user information
+ * @access private
+ */
+authRouter.get("/get-me", authMiddleware, getMeController);
+
+export default authRouter;
 
