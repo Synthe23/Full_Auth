fcdf8094c510b6d9a3b6b02e5698afa58a37bfc3
OmPrakash23 <mishraomm5000@gmail.com>
2026-05-06 10:02:47 +0530

    Set up Express server with CORS and routes

--- server.js
+++ server.js
@@ -1,1 +1,53 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import subjectRouter from "./routes/subject.routes.js";
 
+dotenv.config();
+
+const app = express();
+connectDB();
+
+// CORS FIRST
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "You are currently in the home page of the app!" });
+});
+
+// Auth Router
+app.use("/api/auth", authRouter);
+
+// Subject Router
+app.use("/api/subjects", subjectRouter);
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server is currently running on PORT ${PORT} ✅`)
+);
+
+export default app;
